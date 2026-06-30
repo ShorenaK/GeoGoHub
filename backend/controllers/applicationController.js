f8d4849155f60787b09a0040e9f4ce2478e5c12b
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-06-30 17:15:08 -0400

    applicationcontroller.js folder

---
+++ backend/controllers/applicationController.js
@@ -0,0 +1,14 @@
+/*
+  controllers/applicationController.js
+
+  This file handles request and response logic for membership applications.
+
+  Responsibilities:
+  - Receive application requests from routes.
+  - Call application model functions.
+  - Send JSON responses back to the client.
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
