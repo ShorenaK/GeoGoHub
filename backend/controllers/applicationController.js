fa9dfb77aa3835f86421d3d7a4b99b173fab3a70
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-06-30 17:26:34 -0400

    applicationcontroller.js functions tempaltes

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -13,7 +13,7 @@
   Project: GeoGoHub
 */
 
-import { createApplication } from '../models/applicationModel.js';
+import { createApplication, getAllApplications, getApplicationById, updateApplication, deleteApplication,} from '../models/applicationModel.js';
 
 // Handles creating a new membership application.
 export async function createApplicationController(req, res) {
@@ -26,6 +26,33 @@ export async function createApplicationController(req, res) {
 
 // Handles retrieving all membership applications.
 export async function getAllApplicationsController(req, res) {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// Handles retrieving one membership application by ID.
+export async function getApplicationByIdController(req, res) {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// Handles updating an existing membership application.
+export async function updateApplicationController(req, res) {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// Handles deleting a membership application.
+export async function deleteApplicationController(req, res) {
   try {
 
   } catch (error) {
