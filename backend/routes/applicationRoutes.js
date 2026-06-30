b9c7bdeb6c4bb3fc26b730322ad031789482df78
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-06-30 19:02:55 -0400

    routes folder and applicationcontroller.js file

---
+++ backend/routes/applicationRoutes.js
@@ -0,0 +1,18 @@
+/*
+  routes/applicationRoutes.js
+
+  This file defines API routes for membership applications.
+
+  Responsibilities:
+  - Connect application URLs to controller functions.
+  - Define CRUD endpoints for the applications collection.
+
+  Collection:
+  - applications
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
+
+import express from 'express';
