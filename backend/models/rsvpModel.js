82d39ea417b945d37400f2a8e7b29f77fe5d5ba1
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-01 17:17:24 -0400

    imports for the

--- backend/models/rsvpModel.js
+++ backend/models/rsvpModel.js
@@ -16,3 +16,26 @@
   Course: CS 5610 Web Development
   Project: GeoGoHub
 */
+/*
+  models/rsvpModel.js
+
+  This file handles MongoDB operations for event RSVPs.
+
+  Responsibilities:
+  - Create new RSVPs.
+  - Read RSVPs.
+  - Update RSVPs.
+  - Delete RSVPs.
+
+  Collection:
+  - rsvps
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
+
+import { ObjectId } from 'mongodb';
+import { getDatabase } from '../db/database.js';
+
+const RSVPS_COLLECTION = 'rsvps';
