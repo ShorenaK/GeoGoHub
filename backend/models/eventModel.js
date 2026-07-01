fab1899e776e60f934d3b720e456f1faa6f51402
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-01 15:59:02 -0400

    event model

---
+++ backend/models/eventModel.js
@@ -0,0 +1,18 @@
+/*
+  models/eventModel.js
+
+  This file handles MongoDB operations for curated club events.
+
+  Responsibilities:
+  - Create new events.
+  - Read events.
+  - Update events.
+  - Delete events.
+
+  Collection:
+  - events
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
