7b2190044c83e9fa7199bb3241522a9ae49922b1
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-01 18:46:44 -0400

    rsvpRoutes

---
+++ backend/routes/rsvpRoutes.js
@@ -0,0 +1,16 @@
+/*
+  routes/rsvpRoutes.js
+
+  This file defines API routes for event RSVPs.
+
+  Responsibilities:
+  - Connect RSVP URLs to controller functions.
+  - Define CRUD endpoints for the rsvps collection.
+
+  Collection:
+  - rsvps
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
