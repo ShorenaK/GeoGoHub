a23527e60927a67fe8d405e937318a783af000a8
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-01 17:14:07 -0400

    rsvp model

---
+++ backend/models/rsvpModel.js
@@ -0,0 +1,18 @@
+/*
+  models/rsvpModel.js
+
+  This file handles MongoDB operations for event RSVPs.
+
+  Responsibilities:
+  - Create new RSVPs.
+  - Read RSVPs.
+  - Update RSVPs.
+  - Delete RSVPs.
+
+  Collection:
+  - rsvps
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
