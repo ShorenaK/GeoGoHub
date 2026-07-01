1d8fd23a3e8d3adf4db072709480e0e09d1c1d52
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-01 17:41:08 -0400

    function tempaltes

--- backend/controllers/rsvpController.js
+++ backend/controllers/rsvpController.js
@@ -20,3 +20,28 @@ import {
   getRsvpById,
   updateRsvp,
 } from '../models/rsvpModel.js';
+
+// Handles creating a new RSVP.
+export async function createRsvpController(req, res) {
+
+}
+
+// Handles retrieving all RSVPs.
+export async function getAllRsvpsController(req, res) {
+ 
+}
+
+// Handles retrieving one RSVP by ID.
+export async function getRsvpByIdController(req, res) {
+
+}
+
+// Handles updating an existing RSVP.
+export async function updateRsvpController(req, res) {
+
+}
+
+// Handles deleting an RSVP.
+export async function deleteRsvpController(req, res) {
+
+}
