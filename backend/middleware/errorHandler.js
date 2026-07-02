9954159697bf4075c7da8424cd16d8c2a7dc3f04
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-02 13:20:48 -0400

    erroHandler file that catches applicatin errors

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,19 @@
+/*
+  middleware/errorHandler.js
+
+  This file handles unexpected server errors.
+
+  Responsibilities:
+  - Catch application errors.
+  - Return a consistent JSON error response.
+  - Prevent the server from crashing.
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
+
+// Handles unexpected server errors.
+export function errorHandler(error, req, res, next) {
+
+}
