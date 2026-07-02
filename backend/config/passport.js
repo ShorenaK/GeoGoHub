133787244935ac591eda42bab962b569288e0618
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-02 13:51:12 -0400

    pre the passport.js

---
+++ backend/config/passport.js
@@ -0,0 +1,15 @@
+/*
+  config/passport.js
+
+  This file configures Passport authentication for GeoGoHub.
+
+  Responsibilities:
+  - Configure local login strategy.
+  - Find users by email.
+  - Compare submitted password with stored password.
+  - Serialize and deserialize users for sessions.
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
