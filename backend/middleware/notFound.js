536133d0935da0839df53eaa8b623b53c939ebdd
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-02 13:14:53 -0400

    prep of the notFound.js file

---
+++ backend/middleware/notFound.js
@@ -0,0 +1,14 @@
+/*
+  middleware/notFound.js
+
+  This file handles requests to routes that do not exist.
+
+  Responsibilities:
+  - Catch unmatched API routes.
+  - Return a clean 404 JSON response.
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
+
