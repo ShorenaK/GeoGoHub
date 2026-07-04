6de4a6afc4ea530d64c61012c1be8268365d5749
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-04 19:42:54 -0400

    rsvps  seed file done

--- backend/seed/seedRsvps.js
+++ backend/seed/seedRsvps.js
@@ -10,3 +10,15 @@
   Course: CS 5610 Web Development
   Project: GeoGoHub
 */
+
+import rsvps from '../data/rsvps.json' with { type: 'json' };
+
+const RSVPS_COLLECTION = 'rsvps';
+
+// Inserts mock RSVPs into MongoDB.
+export async function seedRsvps(db) {
+  await db.collection(RSVPS_COLLECTION).deleteMany({});
+  const result = await db.collection(RSVPS_COLLECTION).insertMany(rsvps);
+
+  console.log(`Seeded ${result.insertedCount} RSVPs.`);
+}
