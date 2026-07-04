ff5cbc193f8b2ea1df590ca6efcb92733c53be8a
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-04 19:31:54 -0400

    files prep

---
+++ backend/seed/seedDatabase.js
@@ -0,0 +1,14 @@
+/*
+  seed/seedDatabase.js
+
+  This file runs all GeoGoHub seed scripts.
+
+  Responsibilities:
+  - Connect to MongoDB.
+  - Seed users, applications, events, and RSVPs.
+  - Close the process after seeding.
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
---
+++ backend/seed/seedRsvps.js
@@ -0,0 +1,12 @@
+/*
+  seed/seedRsvps.js
+
+  This file seeds the rsvps collection.
+
+  Collection:
+  - rsvps
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
