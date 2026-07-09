b7832f835cb24fd4d64a49323887f52341ddc9fe
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-09 18:00:50 -0400

    eventPage.jsx

---
+++ frontend/src/pages/EventsPage.jsx
@@ -0,0 +1,20 @@
+/*
+  EventsPage.jsx
+
+  This file defines the events page for GeoGoHub.
+
+  Responsibilities:
+  - Display curated club events.
+  - Allow members to browse upcoming events.
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
+
+// Render the events page.
+function EventsPage() {
+  return <h2>Events Page</h2>;
+}
+
+export default EventsPage;
