2266ba9231b3f58d6e51e3b98ec77488c2e8b659
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-09 17:59:05 -0400

    LoginPage.jsx file

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,21 @@
+/*
+  LoginPage.jsx
+
+  This file defines the login page for GeoGoHub.
+
+  Responsibilities:
+  - Display the login interface.
+  - Allow users to authenticate.
+  - Connect to the backend authentication API.
+
+  Author: Shorena K. Anzhilov
+  Course: CS 5610 Web Development
+  Project: GeoGoHub
+*/
+
+// Render the login page.
+function LoginPage() {
+  return <h2>Login Page</h2>;
+}
+
+export default LoginPage;
