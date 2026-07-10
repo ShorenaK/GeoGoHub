cca89509f9fac1286b4d9ecbde31b90ffce7b70a
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-10 18:24:43 -0400

    header was updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,19 +5,23 @@
 
   Responsibilities:
   - Display the application title.
+  - Display the main navigation.
   - Provide a consistent header across all pages.
-  - Serve as the top section of the application layout.
 
   Author: Shorena K. Anzhilov
   Course: CS 5610 Web Development
   Project: GeoGoHub
 */
 
+import Navbar from './Navbar.jsx';
+
 // Render the application header.
 function Header() {
   return (
     <header>
       <h1>GeoGo Hub</h1>
+
+      <Navbar />
     </header>
   );
 }
