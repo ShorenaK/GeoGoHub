d4bbdb4320d4cc10ca24136c2fc12f1497bd7bbb
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2026-07-14 18:46:15 -0400

    eventList.jsx modifide

--- frontend/src/components/EventList.jsx
+++ frontend/src/components/EventList.jsx
@@ -6,6 +6,7 @@
   Responsibilities:
   - Render a collection of events.
   - Display one EventCard for each event.
+  - Pass RSVP data and actions to each event card.
   - Keep list rendering separate from the page.
 
   Author: Shorena K. Anzhilov
@@ -19,27 +20,50 @@ import EventCard from './EventCard.jsx';
 import '../styles/EventList.css';
 
 // Render a collection of events.
-function EventList({ events }) {
+function EventList({
+  events,
+  canManageRsvps,
+  findRsvpForEvent,
+  onCreateRsvp,
+  onDeleteRsvp,
+  onUpdateRsvp,
+  pendingEventId,
+}) {
   return (
     <div className="event-list">
       {events.map((event) => (
-        <EventCard key={event._id} event={event} />
+        <EventCard
+          key={event._id}
+          event={event}
+          rsvp={findRsvpForEvent(event._id)}
+          canManageRsvps={canManageRsvps}
+          isPending={pendingEventId === event._id}
+          onCreateRsvp={onCreateRsvp}
+          onDeleteRsvp={onDeleteRsvp}
+          onUpdateRsvp={onUpdateRsvp}
+        />
       ))}
     </div>
   );
 }
 
+const eventShape = PropTypes.shape({
+  _id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  capacity: PropTypes.number.isRequired,
+});
+
 EventList.propTypes = {
-  events: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      location: PropTypes.string.isRequired,
-      category: PropTypes.string.isRequired,
-      capacity: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
+  events: PropTypes.arrayOf(eventShape).isRequired,
+  canManageRsvps: PropTypes.bool.isRequired,
+  findRsvpForEvent: PropTypes.func.isRequired,
+  onCreateRsvp: PropTypes.func.isRequired,
+  onDeleteRsvp: PropTypes.func.isRequired,
+  onUpdateRsvp: PropTypes.func.isRequired,
+  pendingEventId: PropTypes.string.isRequired,
 };
 
 export default EventList;
